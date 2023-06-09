2c5aa42b62f02188f4618837c92ceaf7a1df9be2
Ravi Kumar <125804537+raviatkumar@users.noreply.github.com>
2023-06-09 10:17:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, redirect, url_for, flash, jsonify
+import numpy as np
+import pickle as p
+import pandas as pd
+import json
+
+
+app = Flask(__name__)
+
+modelfile = 'models/final_prediction.pickle'    
+
+model = p.load(open(modelfile, 'rb'))
+
+@app.route('/')
+def main():
+    return ('Predict Wine API')
+    
+@app.route('/api/', methods=['POST'])
+def makecalc():
+	j_data = request.get_json()
+
+	prediction = np.array2string(model.predict(j_data))
+	
+	return jsonify(prediction)
+
+
+if __name__ == '__main__':
+
+    
+    app.run(debug=True,host='0.0.0.0')
